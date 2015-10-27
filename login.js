ca50b13a9d4089143a2204e6b11e9ea0146dd0bb
zamatthews <zamatthews@comcast.net>
2015-10-27 13:42:37 -0400

    Reads cookies now
    Added cookie reading to login.js, added a test page to create cookie
    with

--- login.js
+++ login.js
@@ -2,7 +2,20 @@
  function checkLogin(){
 	
  }
+
+function getUsername(){
+	return document.cookie.split('@')[0];
+}
+
  
  function showLoginInfo(){
-	
-            }              
+	document.getElementById("loginForm").innerHTML = "";
+	document.getElementById("loginForm").appendChild(document.createTextNode("Hello, " + getUsername()));
+}              
+
+function createCookieTest(){
+	var user = document.getElementById("user").value;
+	var type = document.getElementById("type").value;
+	document.cookie = user + '@' + type;
+	showLoginInfo();
+}
