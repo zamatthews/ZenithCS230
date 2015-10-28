638e1c374dd2eb170a2b8debcccfa847383fd63e
zamatthews <zamatthews@comcast.net>
2015-10-28 10:44:54 -0400

    Changed cookies
    Now displays nothing if there is no cookie for the document.  To grab
    the type of user(Student, Commisioner, etc.) use the method "getType()"

--- login.js
+++ login.js
@@ -4,18 +4,24 @@
  }
 
 function getUsername(){
-	return document.cookie.split('@')[0];
+	return document.cookie.split(';')[0];
+}
+
+function getType(){
+	return document.cookie.split(';')[1];
 }
 
  
  function showLoginInfo(){
 	document.getElementById("loginForm").innerHTML = "";
+	if(document.cookie.length > 1){
 	document.getElementById("loginForm").appendChild(document.createTextNode("Hello, " + getUsername()));
-}              
+	}
+}
 
 function createCookieTest(){
 	var user = document.getElementById("user").value;
 	var type = document.getElementById("type").value;
-	document.cookie = user + '@' + type;
+	document.cookie = user + ';' + type;
 	showLoginInfo();
 }
