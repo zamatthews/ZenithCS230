86612cf6c92cb915cff6c8611604f01a411e249a
zamatthews <zamatthews@comcast.net>
2015-12-03 19:45:38 -0500

    added more links

--- login.js
+++ login.js
@@ -6,6 +6,10 @@ function isCommissioner(){
 	return (document.cookie.split('@')[1] == "1");
 }
 
+function isHead(){
+	return (document.cookie.split('@')[1] == "2");
+}
+
 function isEligible(){
 	return (document.cookie.split('@')[2] == "1");
 }
@@ -14,7 +18,7 @@ function isEligible(){
 	 side = document.getElementById("sideLogin");
 	 side.innerHTML = "";
 	 var p = document.createElement('p');
-	if(document.cookie.split('@').length == 3){
+	if(document.cookie.split('@').length >= 3){
 		p.appendChild(document.createTextNode("Hello, " + getUsername()));
 	}
 	side.appendChild(p);
@@ -23,9 +27,9 @@ function isEligible(){
 
 function createCookieTest(){
 	var cookieString = document.getElementById("user").value;
-	if(document.getElementsByName("commissioner")[0].checked) 
-		cookieString += "@1";
-	else cookieString += "@0";
+	if(document.getElementsByName("commissioner")[0].checked) cookieString += "@1";
+	else if(document.getElementsByName("commissioner")[1].checked) 	cookieString += "@0";
+	else cookieString += "@2";
 	
 	if(document.getElementsByName("eligible")[0].checked) 
 		cookieString += "@1";
@@ -54,9 +58,19 @@ function populateLinks(){
 	pastLink.appendChild(document.createTextNode("Previous Ballots"));
 	
 	var addLink = document.createElement('a');
-	addLink.href = "./EditElections.html"
+	addLink.href = "./EditElections.html";
 	addLink.setAttribute("id", "sideLink");
 	addLink.appendChild(document.createTextNode("Add Ballots"));
+	
+	var disqualifyLink = document.createElement('a');
+	disqualifyLink.href = "./disqualify.html";
+	disqualifyLink.setAttribute("id", "sideLink");
+	disqualifyLink.appendChild(document.createTextNode("Disqualify student"));
+	
+	var addCommissionerLink = document.createElement('a');
+	addCommissionerLink.href = "./addCommissioner.html";
+	addCommissionerLink.setAttribute("id", "sideLink");
+	addCommissionerLink.appendChild(document.createTextNode("Add/Remove Commissioner"));
 	
 	sideLinks.appendChild(homeLink);
 	sideLinks.appendChild(document.createElement('br'));
@@ -70,7 +84,14 @@ function populateLinks(){
 		sideLinks.appendChild(document.createElement('br'));
 		sideLinks.appendChild(addLink);
 	}
-	
+	if(isHead()){
+		sideLinks.appendChild(document.createElement('br'));
+		sideLinks.appendChild(document.createElement('br'));
+		sideLinks.appendChild(disqualifyLink);
+		sideLinks.appendChild(document.createElement('br'));
+		sideLinks.appendChild(document.createElement('br'));
+		sideLinks.appendChild(addCommissionerLink);
+	}	
 }
 
 function populateTitle(){
